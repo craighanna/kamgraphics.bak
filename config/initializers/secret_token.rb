7418039e7ffbf0b4c7fc490ef3a2f7e0c02a3e73
Craig Hanna <craighanna@gmail.com>
2012-02-08 22:32:46 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kamgraphics::Application.config.secret_token = 'f4343bb4a30ab68f45579daaf26d174318624147312435ce9b43d1a918a1e624b05175ee562734c74864ecb0894e487af545a27d1d7b8cd55388055b62f3ccf9'
